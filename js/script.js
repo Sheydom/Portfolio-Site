ee03790b0b04aafcbe794b944e31544b8758f43e
Dom <dominic.knabe@gmail.com>
2025-03-08 06:14:51 +1100

    set upt header and footer components and form scss and form page

--- js/script.js
+++ js/script.js
@@ -1,23 +1,30 @@
+const main = document.querySelector(".main");
+const bio = document.querySelector(".bio");
 
-const bio = document.querySelector(".bio")
-const nav = document.querySelector(".nav")
-const header = document.querySelector(".header")
+
 
 window.addEventListener("DOMContentLoaded",()=>{
-    fetch("header.html")
+    fetch("components/header.html")
     .then(response=> response.text())
     .then(data=>{
-        // very important to have t he setTimeout here because of the asyncrhonus fetch browser tries to run addeventlistener on hamburger menu without being loaded in the DOM.
-        setTimeout(()=>{
-            document.getElementById("nav").innerHTML=data
+        
+        
+            document.getElementById("nav").innerHTML=data;
             const hamburgerMenu = document.getElementById("hamburgerMenu");
             const nav__list  =   document.querySelector(".nav__list");
             hamburgerMenu.addEventListener("click", ()=>{
                 nav__list.classList.contains("show")? nav__list.classList.remove("show") : nav__list.classList.add("show");
         },0)
 
-});
+            updatePadding();
     })
+    
+    fetch("components/footer.html")
+    .then(response=>response.text())
+    .then(data=>{
+        document.getElementById("footer").innerHTML=data;
+    })
+    
 })
 
 
@@ -27,11 +34,29 @@ window.addEventListener("resize",()=>{
         nav__list.classList.remove("show")
 })
  
+// function to have the content always below the navbar as the navbar is fixed and out of the DOM flow so use js to calculate the propper padding-top accordlingy to the height of the header navbar 
+
 const updatePadding =()=>{
-    bio.style.paddingTop= `${header.offsetHeight}px`;
+   
+    const header = document.querySelector(".header");
+    // ensure header exists
+    if(header){
+      const  headerHeight=`${header.offsetHeight}px`
+// apply padding to bio top if it exists
+      if(bio){
+        bio.style.paddingTop=headerHeight;
+      }
+    //   apply padding to main top if it exists
+      if(main){
+        main.style.paddingTop=headerHeight;
+      }
+
+    } 
 }
 
-updatePadding();
+
+    
+
 
 window.addEventListener("resize",updatePadding);
 
